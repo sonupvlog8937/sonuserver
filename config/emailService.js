1d04c3cac68e775da319e7e3a7a4c19e8574cf10
Sonu Kumar <sonupvlog8937@gmail.com>
2026-03-06 23:00:17 +0530

    587

--- config/emailService.js
+++ config/emailService.js
@@ -26,19 +26,19 @@ function createTransporter() {
 async function sendEmail(to, subject, text, html) {
   try {
     const transporter = createTransporter();
-   
+
     const info = await transporter.sendMail({
-       from: process.env.EMAIL,
-      to,
+      from: process.env.EMAIL,
+      to: Array.isArray(to) ? to.join(",") : to, // ✅ Array ko string mein convert
       subject,
       text,
       html,
     });
     return { success: true, messageId: info.messageId };
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error("Error sending email:", error);
     return { success: false, error: error.message };
   }
 }
 
-export {sendEmail};
+export { sendEmail };
